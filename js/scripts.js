13e00a4ed73ba69113b63410ed40bb85284176b4
MichaelOdera <michogelira@gmail.com>
2019-12-15 12:48:56 +0300

    add click event listener on button

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,13 @@
-// var deleteCartItem = document.getElementByClassName("btn-danger");
-// console.log(deleteCartItem);
+
+/*Obtain all the delete buttons in the cart*/
+var deleteCartItemButtons = document.getElementsByClassName("btn-danger");
+console.log(deleteCartItemButtons);
+for(var i = 0; i < deleteCartItemButtons.length; i++){
+    var button = deleteCartItemButtons[i];
+    /*Listen for click then remove*/
+    button.addEventListener('click', function(event){
+        console.log("clicked");
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.remove();
+    })
+}
