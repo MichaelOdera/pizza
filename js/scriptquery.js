9cd3515b4022276135c5cd3c3744076868f4b0c4
MichaelOdera <michogelira@gmail.com>
2019-12-16 07:46:05 +0300

    added the checkout  button function

--- js/scriptquery.js
+++ js/scriptquery.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
-   $(".add-to-cart").one('click', function(){
-       $("#fa-shopping").append("You have items in cart");
-   });
-
-   $("#fa-shopping").one('click', function(){
-       $(".shop").toggle();
-   });
-
-   $(".go-to-cart").one('click', function(){
-       $(".container-fluid").hide();
-       $(".cart").show();
-   });
-
-   $(".back-to-shop").one('click', function(){
-       $(".container-fluid").show();
-       $(".cart").hide();
-   })
+    $("#fa-shopping").one('click', function(){
+        $(".shop").toggle();
+       
+    });
+    
+    $(".go-to-cart").one('click', function(){
+        $(".container-fluid").hide();
+        $(".cart").show();
+    });
+    
+    $(".back-to-shop").one('click', function(){
+        $(".container-fluid").show();
+        $(".cart").toggle();
+    });
 });
